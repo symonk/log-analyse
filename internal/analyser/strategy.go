ffbd3768a5290fa18110369e9c7b4a62c8e84460
Simon K <jackofspaces@gmail.com>
2024-07-21 20:43:21 +0100

    add sequential scanning and new `strategyMap`

--- internal/analyser/strategy.go
+++ internal/analyser/strategy.go
@@ -1,5 +1,9 @@
 package analyser
 
+var strategyMap = map[string]func(loadedFile LoadedFile) []string{
+	"sequential": seqScanStrategyFn,
+}
+
 // seqScanStrategyFn processes a file sequentially
 // using a single thread and reports matches against
 // any of it's patterns
