9900c412c5f9b794c9e6380f5cd1a4ce4ffa7c13
Simon K <jackofspaces@gmail.com>
2024-07-21 10:36:29 +0100

    simplify `config_test.go`

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -11,7 +11,6 @@ import (
 var basic = []byte(`
 ---
 files:
-  # A glob based folder lookup
   - glob: "~/logs/*.txt"
     threshold:
       hits: 5
@@ -20,7 +19,7 @@ files:
         - ".*FATAL.*"
         - ".*payment failed.*"
       notify: "email"
-  # An explicit log file
+
   - glob: "~/logs/foo.log"
     threshold:
       hits: 1
@@ -32,7 +31,7 @@ files:
 `)
 
 func TestCanUnmarshalConfigSuccessfully(t *testing.T) {
-	c, err := loadConfigFile(t, basic)
+	c, err := loadConfigFile(basic)
 	assert.Nil(t, err)
 	assert.Len(t, c.Files, 2)
 }
@@ -45,10 +44,14 @@ func TestReturnGlobs(t *testing.T) {
 
 // loadConfigFile streams a byte slice into a viper config and
 // unmarshals it into the Config object.
-func loadConfigFile(t *testing.T, b []byte) (*Config, error) {
+func loadConfigFile(b []byte) (*Config, error) {
 	var c *Config
 	viper.SetConfigType("yaml")
-	viper.ReadConfig(bytes.NewBuffer(b))
-	err := viper.Unmarshal(&c)
-	return c, err
+	if err := viper.ReadConfig(bytes.NewBuffer(b)); err != nil {
+		return c, err
+	}
+	if err := viper.Unmarshal(&c); err != nil {
+		return c, err
+	}
+	return c, nil
 }
--- internal/files/locator.go
+++ internal/files/locator.go
@@ -4,6 +4,10 @@ type Collector interface {
 	Locate() error
 }
 
+// FileCollector is responsible for taking globs and their
+// paired configuration and flattening those glob patterns
+// into actual actionable files and the configs that apply
+// to them
 type FileCollector struct {
 }
 
