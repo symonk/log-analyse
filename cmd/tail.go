135b5a53dec48d3d5beca2d8a216451c512b8fc1
Simon K <jackofspaces@gmail.com>
2024-07-27 19:00:31 +0100

    basic boilerate plate for `--tail` mode

--- cmd/tail.go
+++ cmd/tail.go
@@ -1,7 +1,11 @@
 package cmd
 
 import (
+	"fmt"
+	"sync"
+
 	"github.com/spf13/cobra"
+	"github.com/symonk/log-analyse/internal/files"
 	"github.com/symonk/log-analyse/internal/prof"
 )
 
@@ -10,7 +14,7 @@ var tailCmd = &cobra.Command{
 	Use:   "tail",
 	Short: "Live tailing of log files",
 	Long:  ``,
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
 		/*
 			Parse globs into concrete files
 			Flatten the files to remove any duplicates?
@@ -18,6 +22,17 @@ var tailCmd = &cobra.Command{
 		if profile {
 			defer prof.RunProf()()
 		}
+		fileLocator := files.NewFileLocator(cfg)
+		squashedFiles, err := fileLocator.Locate()
+		if err != nil {
+			return fmt.Errorf("error when resolving glob patterns to files %w", err)
+		}
+		var wg sync.WaitGroup
+		wg.Add(len(squashedFiles))
+		for _, f := range squashedFiles {
+			fmt.Println(f.Path)
+		}
+		return nil
 	},
 }
 
--- internal/monitor/filemon.go
+++ internal/monitor/filemon.go
@@ -1,1 +1,13 @@
 package monitor
+
+import "os"
+
+type Watcher interface{}
+
+// Filemon is responsible for tailing an arbitrary number of files
+type Filemon struct {
+}
+
+func (f *Filemon) Watch(file *os.File) {
+
+}
